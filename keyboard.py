9a31fd76910172b5ce166a8bee8e11c399e74e5f
vladimirsakhatskii <147995810+vladimirsakhatskii@users.noreply.github.com>
2023-11-11 18:04:36 +0600

    Add files via upload

--- keyboard.py
+++ keyboard.py
@@ -1,11 +1,16 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
-kb = ReplyKeyboardMarkup(resize_keyboard=True)
-b1 = KeyboardButton(text='Помощь')
-b2 = KeyboardButton(text='Описание')
-b3 = KeyboardButton(text='Откликнуться')
-b4 = KeyboardButton(text='Позвонить')
+kb_admin = ReplyKeyboardMarkup(resize_keyboard=True)
+b1 = KeyboardButton(text='/help_admin')
+b2 = KeyboardButton(text='/description')
+b3 = KeyboardButton(text='Создать вопрос')
+kb_admin.add(b1,b2)
+kb_admin.add(b3)
 
-kb.add(b1,b2)
-kb.add(b3)
-kb.add(b4)
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+b_1 = KeyboardButton(text='/help')
+b_2 = KeyboardButton(text='/description')
+b_3 = KeyboardButton(text='Задать вопрос')
+kb.add(b_1,b_2)
+kb.add(b_3)
