f969d008d9eca20d1d513299cf2decc00a31aa54
vladimirsakhatskii <147995810+vladimirsakhatskii@users.noreply.github.com>
2023-11-11 18:04:49 +0600

    Add files via upload

--- database.py
+++ database.py
@@ -1,22 +1,24 @@
 import sqlite3 as sq
 
-db = sq.connect('tg.db')
-cur = db.cursor()
-
 async def db_start():
-    cur.execute("CREATE TABLE IF NOT EXISTS accounts("
-                "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                "user_name TEXT, "
-                "user_surname TEXT, "
-                "user_email varchar, "
-                "user_university TEXT, "
-                "user_phone_number TEXT)")
+    global db, cur
+    db = sq.connect('tg.db')
+    cur = db.cursor()
+
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS admin (
+    id INTEGER PRIMARY KEY,
+    question TEXT NOT NULL,
+    key_world TEXT NOT NULL,
+    answer TEXT NOT NULL
+    )
+    ''')
     db.commit()
+
 
 async def add_item(state):
     async with state.proxy() as data:
-        cur.execute("INSERT INTO accounts (user_name, user_surname, user_email, user_university, user_phone_number) VALUES (?, ?, ?, ?, ?)",
-                    (data['user_name'], data['user_surname'], data['user_email'], data['user_university'], data['user_phone_number']))
+        cur.execute(
+                "INSERT INTO admin (question, key_world, answer) VALUES (?, ?, ?)",
+                (data['question'], data['key_world'], data['answer']))
         db.commit()
-
-
