7ae871b9ed6b61a1618a66df126f89347ae9aba6
vladimirsakhatskii <147995810+vladimirsakhatskii@users.noreply.github.com>
2023-11-11 18:04:00 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,123 +1,117 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.dispatcher import FSMContext
 from dotenv import load_dotenv
 from app import keyboard as kb
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
 from app import database as db
 import os
+
 
 storage = MemoryStorage()
 load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot, storage=storage)
 
+
 async def on_startup(_):
     await db.db_start()
-    print('Бот успешно запущен!')
+    print('База данных успешно запущена!')
 
 
-class VAL (StatesGroup):
-    user_name = State()
-    user_surname = State()
-    user_email = State()
-    user_university = State()
-    user_phone_number = State()
+HELP_ADMIN = '''
+/help_admin - команды бота,
+/description - пока не придумал
+Создать вопросы - сделать список, часто задаваемых вопросов
+'''
 
 
-HELP = """
-Помощь - список комманд
-Откликнуться - откликнуться на вакансию
-Описание - описание бота
-Позвонить - показывает номера, куда можно позвонить по вопросам
-/cat - прикольный стикер с котёнком
-"""
-
-NOMBER = """
-8913.......
-8904.......
-"""
+HELP = '''
+/help - команды бота,
+/description - пока не придумал
+Задать вопрос - написать вопрос, который интересует
+'''
 
 
-@dp.message_handler (text = 'Помощь')
-async def help_menu(message: types.Message):
-    await message.reply(text=HELP)
+class QUESTION (StatesGroup):
+    question = State()
+    key_world = State()
+    answer = State()
 
-@dp.message_handler (text = 'Позвонить')
-async def help_menu(message: types.Message):
-    await message.answer(text='Вот номера телефонов')
-    await message.reply(text=NOMBER)
-
-@dp.message_handler (text = 'Откликнуться')
-async def respond(message: types.Message):
-    await VAL.user_name.set()
-    await message.answer(text='Введите имя')
-
-@dp.message_handler (state = VAL.user_name)
-async def add_respond(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['user_name'] = message.text
-    await message.answer(text = 'Введите фамилию')
-    await VAL.next()
-
-@dp.message_handler (state = VAL.user_surname)
-async def add_respond(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['user_surname'] = message.text
-    await message.answer(text = 'Введите почту')
-    await VAL.next()
-
-
-@dp.message_handler (state = VAL.user_email)
-async def add_respond(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['user_email'] = message.text
-    await message.answer(text = 'Введите название вуза, который закончили')
-    await VAL.next()
-
-@dp.message_handler (state = VAL.user_university)
-async def add_respond(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['user_university'] = message.text
-    await message.answer(text = 'Введите номер телефона, чтобы мы могли с вами связаться')
-    await VAL.next()
-
-@dp.message_handler (state = VAL.user_phone_number)
-async def add_respond(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['user_phone_number'] = message.text
-    await db.add_item(state)
-    await message.answer(text = 'Ваши данные были добавлены в базу данных')
-    await message.answer(text = 'Ждите с вами свяжутся')
-    await state.finish()
 
 @dp.message_handler (commands=['start'])
 async def start_menu(message: types.Message):
-    await bot.send_message(
-        text=f'{message.from_user.first_name}, добро пожаловать!',
-        chat_id=message.from_user.id,
-        reply_markup=kb.kb)
-    await message.answer_sticker('CAACAgIAAxkBAAIBYWUr3Tb-uAtGQA9i9lXseYkslWU0AALVDAACKxeYSs2dNpuTnoMBMAQ')
-    await message.answer(text='В данном боте вы можете откликнуться на вакансию, вписав боту ваши данные для работодателя')
+    if (message.from_user.id == int(os.getenv('ADMIN_ID_1'))) or (message.from_user.id == int(os.getenv('ADMIN_ID_2'))):
+        await bot.send_message(
+            text=f'{message.from_user.first_name}, добро пожаловать в окно организатора!',
+            chat_id=message.from_user.id,
+            reply_markup=kb.kb_admin)
+        await message.answer_sticker('CAACAgIAAxkBAAJbgWVPVqh6R2jme1XPYujOay8f5mxwAALyOwACO4Z5St1QvCGWR8SGMwQ')
+    else:
+        await bot.send_message(
+            text=f'{message.from_user.first_name}, добро пожаловать в окно участника!',
+            chat_id=message.from_user.id,
+            reply_markup=kb.kb)
+        await message.answer_sticker('CAACAgIAAxkBAAJbgWVPVqh6R2jme1XPYujOay8f5mxwAALyOwACO4Z5St1QvCGWR8SGMwQ')
 
 
-@dp.message_handler (text = 'Описание')
-async def description(message: types.Message):
-    await message.answer(text='В данном боте вы можете откликнуться на вакансию, вписав боту ваши данные для работодателя')
-    await message.delete()
+@dp.message_handler (commands=['description'])
+async def def_description(message: types.Message):
+    await message.reply(text='пока не придумал')
 
 
-@dp.message_handler (commands=['cat'])
-async def cat_sticker(message: types.Message):
-    await message.answer_sticker('CAACAgIAAxkBAAIBf2UsByFX7L9tvSbqYuohdMjpUsJWAAKmJgAC0EQ5SmMpKc4MwbzTMAQ')
-    await message.delete()
+@dp.message_handler (commands=['help_admin'])
+async def def_help(message: types.Message):
+    await message.reply(text=HELP_ADMIN)
+
+
+@dp.message_handler (commands=['help'])
+async def def_help(message: types.Message):
+    await message.reply(text=HELP)
+
+
+@dp.message_handler (text = 'Создать вопрос')
+async def def_question(message: types.Message):
+    await QUESTION.question.set()
+    await message.reply(text='Напишите вопрос')
+
+
+@dp.message_handler (state = QUESTION.question)
+async def add_question(message: types.Message,  state: FSMContext):
+    async with state.proxy() as data:
+        data['question'] = message.text
+    await message.answer(text = 'Напишите ключевое слово')
+    await QUESTION.next()
+
+
+@dp.message_handler (state = QUESTION.key_world)
+async def add_question(message: types.Message,  state: FSMContext):
+    async with state.proxy() as data:
+        data['key_world'] = message.text
+    await message.answer(text='Напишите ответ на вопрос')
+    await QUESTION.next()
+
+
+@dp.message_handler (state = QUESTION.answer)
+async def add_question(message: types.Message,  state: FSMContext):
+    async with state.proxy() as data:
+        data['answer'] = message.text
+    await db.add_item(state)
+    await message.answer(text='Вопрос создан')
+    await message.answer_sticker('CAACAgIAAxkBAAJbg2VPVsrAvcqdL1rUZRigLS9ACpeeAALMNgAC9GmASlZMmFp0lZCIMwQ')
+    await state.finish()
+
+
+@dp.message_handler (text = 'Задать вопрос')
+async def def_question(message: types.Message):
+    await message.reply(text='Пока не придумал')
 
 
 @dp.message_handler()
-async def error(message: types.Message):
-    await message.reply(text='^^5@$**&%.......Я тебя не понимаю.......&%$#***&')
-    await message.answer(text='Такой команды нету')
+async def def_error(message: types.Message):
+    await message.answer(text='Я тебя не понимаю')
 
 
 if __name__ == '__main__':
+    print('Бот успешно запущен!')
     executor.start_polling(dp, on_startup = on_startup)
